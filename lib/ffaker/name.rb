3d536ef9970f376f33c68a6c4673c4887ff23fad
Chris Spagnuolo <chris@topify.com>
2015-08-18 19:03:47 -0700

    Add male/female name methods

--- lib/ffaker/name.rb
+++ lib/ffaker/name.rb
@@ -6,6 +6,8 @@ module FFaker
     extend self
 
     PREFIXES = %w(Mr. Mrs. Ms. Miss Dr.)
+    MALE_PREFIXES = %w(Mr. Dr.)
+    FEMALE_PREFIXES = %w(Mrs. Ms. Miss Dr.)
     SUFFIXES = %w(Jr. Sr. I II III IV V MD DDS PhD DVM)
 
     def name
@@ -16,8 +18,32 @@ module FFaker
       end
     end
 
+    def male_name
+      case rand(10)
+      when 0 then "#{male_prefix} #{male_first_name} #{last_name}"
+      when 1 then "#{male_first_name} #{last_name} #{suffix}"
+      else        "#{male_first_name} #{last_name}"
+      end
+    end
+
+    def female_name
+      case rand(10)
+      when 0 then "#{female_prefix} #{female_first_name} #{last_name}"
+      when 1 then "#{female_prefix} #{female_first_name} #{last_name}"
+      else        "#{female_first_name} #{last_name}"
+      end
+    end
+
     def first_name
       FIRST_NAMES.sample
+    end
+
+    def male_first_name
+      MALE_FIRST_NAMES.sample
+    end
+
+    def female_first_name
+      FEMALE_FIRST_NAMES.sample
     end
 
     def last_name
@@ -26,6 +52,14 @@ module FFaker
 
     def prefix
       PREFIXES.sample
+    end
+
+    def male_prefix
+      MALE_PREFIXES.sample
+    end
+
+    def female_prefix
+      FEMALE_PREFIXES.sample
     end
 
     def suffix
